723b06ebd65a4a3940b692791c8369351e37160c
Alex OToole <tooler1999@gmail.com>
2019-09-26 11:57:33 +0100

    initial commit

---
+++ Assets/FactorialFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactorialFilter
+{
+    void Factorial(int[] t_input)
+    {
+        int[] output = new int [t_input.Length];
+        int temp = 0;
+        int fact = 0;
+        for (int i = 0; i < t_input.Length; i++)
+        {
+            temp = t_input[i];
+            while (temp != 0)
+            {
+                fact = temp * (temp - 1);
+                temp -= 1;
+            }
+            output[i] = fact;
+        }
+    }
+}
